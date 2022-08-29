b786aec394977364bca1eb2a9ab9836db81eb9da
yotamsMoveo <106738399+yotamsMoveo@users.noreply.github.com>
2022-08-29 16:24:23 +0300

    add jwt

---
+++ CommandsService/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("Admin")]
+        [Authorize(Roles ="Administretor")]
+        public IActionResult AdminEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"hi {currentUser.GivenName}");
+        }
+
+        [HttpGet("Sellers")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult SellersEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"hi {currentUser.GivenName}");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value,
+                };
+            }
+            return null;
+        }
+
+    }
+}
