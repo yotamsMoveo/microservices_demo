4979a3bb2397ea71bc308b5ef0becfbb592fb461
yotamsMoveo <106738399+yotamsMoveo@users.noreply.github.com>
2022-08-29 16:25:42 +0300

    Add files via upload

---
+++ CommandsService/Models/UserConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CommandsService.Models
+{
+    public class UserConstants
+    {
+        public UserConstants()
+        {
+        }
+
+        public static List<UserModel> users = new List<UserModel>()
+        {
+            new UserModel(){Username="yotam",EmailAddress="jason.admin@email.com",Password="1234",GivenName="yotam",Surname="saadon",Role="Administrator"},
+            new UserModel(){Username="gal",EmailAddress="jason.admin@email.com",Password="1234",GivenName="gal",Surname="saadon",Role="Seller"}
+        };
+    }
+}
+
